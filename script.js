043a42ff0889da8c0a5953290e6c49acc7483875
Marvin Rühe <marvin@server-fish.de>
2019-05-17 18:59:10 +0200

    Add english version

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("show_impressum").addEventListener("click", function(){
+        document.getElementById("impressum").classList.toggle("hidden_impressum")
+    });
+});
