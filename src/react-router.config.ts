a416e6b59fd34ed1e52ed216a8a684a61cc86a63
Marvin Rühe <471338+Marv51@users.noreply.github.com>
2026-06-14 17:48:28 +0200

    Add containerized dev setup (#30)

--- src/react-router.config.ts
+++ src/react-router.config.ts
@@ -5,4 +5,11 @@ export default {
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
   prerender: true,
+  future: {
+    v8_middleware: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+    v8_passThroughRequests:true,
+    v8_trailingSlashAwareDataRequests: true,
+  },
 } satisfies Config;
